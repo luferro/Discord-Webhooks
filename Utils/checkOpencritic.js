fcf98d7c514da93f56b001b16f597659ce07b130
Luís Ferro <lfaferro@live.com.pt>
2021-06-06 19:43:36 +0100

    Update checkOpencritic.js

--- Utils/checkOpencritic.js
+++ Utils/checkOpencritic.js
@@ -4,7 +4,7 @@ export const checkOpencritic = async() => {
     try {
         const res = await fetch('https://www.reddit.com/r/Games/search.json?q=flair_name%3A%22Review%20Thread%22&restrict_sr=1&sort=new');
         const game = res.headers.get('Content-Type')?.includes('application/json') ? await res.json() : await res.text();
-        if(!res.ok) return console.log(`${res.status} - ${review}`);
+        if(!res.ok) return console.log(`${res.status} - ${game}`);
 
         const selftext = game.data.children[0].data.selftext.split('\n');
         const opencritic = selftext.find(item => item.includes('https://opencritic.com/game/'));
